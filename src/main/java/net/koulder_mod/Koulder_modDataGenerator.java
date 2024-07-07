5eb7407d477c5274e89dfc3daf4da0b229f1f831
Koulder <Cx330Max@outlook.com>
2024-07-07 10:38:25 +0800

    Initial commit

---
+++ src/main/java/net/koulder_mod/Koulder_modDataGenerator.java
@@ -0,0 +1,11 @@
+package net.koulder_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Koulder_modDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
